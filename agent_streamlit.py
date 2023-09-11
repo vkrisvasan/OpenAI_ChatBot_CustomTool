6d378bf37ad82ec9c6ca9ffd6c4705a48267920b
Krishnan Vijayaraghavan <10602190+vkrisvasan@users.noreply.github.com>
2023-09-11 08:10:24 +0530

    Add files via upload

---
+++ agent_streamlit.py
@@ -0,0 +1,78 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+
+from langchain.agents import AgentExecutor
+
+import callbacks
+
+load_dotenv()
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+from chain_setup import setup_agent
+
+QUESTION_HISTORY: str = 'question_history'
+
+
+def init_stream_lit():
+    title = "Chat Functions Introduction"
+    # sidebar contents
+    with st.sidebar:
+        st.title('OpenAI LLM based chatbot with custom tools like arxiv, wikipedia, pubmed, duckduckgo search')
+        st.markdown('''
+        ## The application is created using:
+
+        - [OpenAI](https://openai.com/)
+        - [Langchain](https://docs.langchain.com/docs/) 
+        - [Streamlit](https://streamlit.io/)
+        - [arxiv](https://arxiv.org/)
+        - [duckduckgo](https://duckduckgo.com/)
+        - [pubmed](https://pubmed.ncbi.nlm.nih.gov/)
+        - [wikipedia](https://www.wikipedia.org/)
+
+        ## About me:
+        - [Linkedin](https://www.linkedin.com/in/vkrisvasan/)
+
+        ''')
+    #st.set_page_config(page_title=title, layout="wide")
+    agent_executor: AgentExecutor = prepare_agent()
+    st.header(title)
+    if QUESTION_HISTORY not in st.session_state:
+        st.session_state[QUESTION_HISTORY] = []
+    intro_text()
+    simple_chat_tab, historical_tab = st.tabs(["Simple Chat", "Session History"])
+    with simple_chat_tab:
+        user_question = st.text_input("Your question")
+        with st.spinner('Please wait ...'):
+            try:
+                response = agent_executor.run(user_question, callbacks=[callbacks.StreamlitCallbackHandler(st)])
+                st.write(f"{response}")
+                st.session_state[QUESTION_HISTORY].append((user_question, response))
+            except Exception as e:
+                st.error(f"Error occurred: {e}")
+    with historical_tab:
+        for q in st.session_state[QUESTION_HISTORY]:
+            question = q[0]
+            if len(question) > 0:
+                st.write(f"Q: {question}")
+                st.write(f"A: {q[1]}")
+
+
+def intro_text():
+    with st.expander("Click to see application info:"):
+        st.write(f"""Ask questions about:
+- [Wikipedia](https://www.wikipedia.org/) Content
+- Scientific publications ([pubmed](https://pubmed.ncbi.nlm.nih.gov) and [arxiv](https://arxiv.org))
+- Mathematical calculations
+- Search engine content ([DuckDuckGo](https://duckduckgo.com/))
+- Meditation related events (Custom Tool)
+    """)
+
+
+@st.cache_resource()
+def prepare_agent() -> AgentExecutor:
+    return setup_agent()
+
+
+if __name__ == "__main__":
+    init_stream_lit()
